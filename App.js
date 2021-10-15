73f72b0bffd5deb4a8ee3c9fced4d51b60505693
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-15 11:31:00 -0300

    Añade limpieza y componente formulario

--- App.js
+++ App.js
@@ -1,16 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Formulario from './components/Formulario/Formulario';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Formulario/>
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
