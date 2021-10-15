2975e6072d6e9de0a41a60342ac7f8bcc8a53079
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-15 11:50:58 -0300

    Añde mejoras en el formulario

--- components/Formulario/Formulario.js
+++ components/Formulario/Formulario.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Text, View, TextInput, StyleSheet} from 'react-native'
+import {Text, View, TextInput, StyleSheet, TouchableHighlight} from 'react-native'
 import { Picker } from '@react-native-community/picker'
 
 const Formulario = ( ) =>{
@@ -7,14 +7,14 @@ const Formulario = ( ) =>{
         <>
             <View style={styles.formulario}>
                 <View>
-                    <TextInput 
+                    <TextInput
+                        style={styles.input}
                         placeholder="Ciudad"
                         placeholderTextColor="#666"
                     />
-                    <Text>Formulario</Text>
                 </View>
                 <View>
-                    <Picker>
+                    <Picker itemStyle={{height:120, backgroundColor:'#FFF'}}>
                         <Picker.Item label="-- Seleccione un país --" value=""/>
                         <Picker.Item label="Estados Unidos" value="US"/>
                         <Picker.Item label="Argentina" value="AR"/>
@@ -25,14 +25,39 @@ const Formulario = ( ) =>{
                         <Picker.Item label="Perú" value="PE"/>
                     </Picker>
                 </View>
+                <TouchableHighlight>
+                    <View
+                        style={styles.btnBuscar}
+                    >
+                        <Text style={styles.textBuscar}>Buscar Clima</Text>
+                    </View>
+                </TouchableHighlight>
             </View>
         </>
     )
 }
 
 const styles = StyleSheet.create ({
-    formulario: {
-        marginTop:20
+    input: {
+        padding:10,
+        height:50,
+        backgroundColor:'#FFF',
+        fontSize:20,
+        marginBottom:20,
+        textAlign:'center'
+    },
+    btnBuscar: {
+        marginTop:50,
+        backgroundColor:'#000',
+        padding:10,
+        justifyContent:'center'
+    },
+    textBuscar:{
+        color:'#FFF',
+        fontWeight:'bold',
+        textTransform:'uppercase',
+        textAlign:'center',
+        fontSize:18
     }
 })
 export default Formulario;
