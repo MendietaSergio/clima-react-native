2bf747a06b89f881c20f1b7c666f3983e898bf4c
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-21 09:33:49 -0300

    Añade animaciones al boton 'buscar clima'

--- components/Formulario/Formulario.js
+++ components/Formulario/Formulario.js
@@ -1,63 +1,97 @@
-import React from "react";
-import {Text, View, TextInput, StyleSheet, TouchableHighlight} from 'react-native'
-import { Picker } from '@react-native-community/picker'
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Animated,
+  Touchable,
+} from "react-native";
+import { Picker } from "@react-native-community/picker";
 
-const Formulario = ( ) =>{
-    return ( 
-        <>
-            <View style={styles.formulario}>
-                <View>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Ciudad"
-                        placeholderTextColor="#666"
-                    />
-                </View>
-                <View>
-                    <Picker itemStyle={{height:120, backgroundColor:'#FFF'}}>
-                        <Picker.Item label="-- Seleccione un país --" value=""/>
-                        <Picker.Item label="Estados Unidos" value="US"/>
-                        <Picker.Item label="Argentina" value="AR"/>
-                        <Picker.Item label="México" value="MX"/>
-                        <Picker.Item label="Colombia" value="CO"/>
-                        <Picker.Item label="Costa Rica" value="CR"/>
-                        <Picker.Item label="España" value="ES"/>
-                        <Picker.Item label="Perú" value="PE"/>
-                    </Picker>
-                </View>
-                <TouchableHighlight>
-                    <View
-                        style={styles.btnBuscar}
-                    >
-                        <Text style={styles.textBuscar}>Buscar Clima</Text>
-                    </View>
-                </TouchableHighlight>
-            </View>
-        </>
-    )
-}
+const Formulario = () => {
+  //solo la variable del estado
+  const [animacionBtn] = useState(new Animated.Value(1));
+  const animacionEntrada = () => {
+    Animated.spring(animacionBtn, {
+      toValue: 0.9,
+    }).start();
+  };
+  const animacionSalida = () => {
+    Animated.spring(animacionBtn, {
+      toValue: 1,
+      friction: 4,
+      tension:30
+    }).start();
+  };
+  const estilosAnimacion = {
+    transform: [{ scale: animacionBtn }],
+  };
+  const animacionEntrada = () => {
+    console.log("entrada..");
+  };
+  const animacionSalida = () => {
+    console.log("salida...");
+  };
 
-const styles = StyleSheet.create ({
-    input: {
-        padding:10,
-        height:50,
-        backgroundColor:'#FFF',
-        fontSize:20,
-        marginBottom:20,
-        textAlign:'center'
-    },
-    btnBuscar: {
-        marginTop:50,
-        backgroundColor:'#000',
-        padding:10,
-        justifyContent:'center'
-    },
-    textBuscar:{
-        color:'#FFF',
-        fontWeight:'bold',
-        textTransform:'uppercase',
-        textAlign:'center',
-        fontSize:18
-    }
-})
+  return (
+    <>
+      <View style={styles.formulario}>
+        <View>
+          <TextInput
+            style={styles.input}
+            placeholder="Ciudad"
+            placeholderTextColor="#666"
+          />
+        </View>
+        <View>
+          <Picker itemStyle={{ height: 120, backgroundColor: "#FFF" }}>
+            <Picker.Item label="-- Seleccione un país --" value="" />
+            <Picker.Item label="Estados Unidos" value="US" />
+            <Picker.Item label="Argentina" value="AR" />
+            <Picker.Item label="México" value="MX" />
+            <Picker.Item label="Colombia" value="CO" />
+            <Picker.Item label="Costa Rica" value="CR" />
+            <Picker.Item label="España" value="ES" />
+            <Picker.Item label="Perú" value="PE" />
+          </Picker>
+        </View>
+        <TouchableWithoutFeedback
+          //cuando lo precionas y cuando lo sueltas
+          onPressIn={() => animacionEntrada()}
+          onPressOut={() => animacionSalida()}
+        >
+          <Animated.View style={styles.btnBuscar, estilosAnimacion}>
+            <Text style={styles.textBuscar}>Buscar Clima</Text>
+          </Animated.View>
+        </TouchableWithoutFeedback>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    padding: 10,
+    height: 50,
+    backgroundColor: "#FFF",
+    fontSize: 20,
+    marginBottom: 20,
+    textAlign: "center",
+  },
+  btnBuscar: {
+    marginTop: 50,
+    backgroundColor: "#000",
+    padding: 10,
+    justifyContent: "center",
+  },
+  textBuscar: {
+    color: "#FFF",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    textAlign: "center",
+    fontSize: 18,
+  },
+});
 export default Formulario;
