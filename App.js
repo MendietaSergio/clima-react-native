3011b9a84de891fe1b6aab3e1e42253366e11ca8
Mendieta Sergio <mendieta.sergio.1995@gmail.com>
2021-10-21 11:00:32 -0300

    Añade conexión con la api

--- App.js
+++ App.js
@@ -1,23 +1,58 @@
-import React, {useState} from "react";
-import { StyleSheet, Text, View, TouchableWithoutFeedback, Keyboard } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+} from "react-native";
 import Formulario from "./components/Formulario/Formulario";
 
 export default function App() {
   const [busqueda, setBusqueda] = useState({
-    ciudad: '',
-    pasi:''
-  })
-  const ocultarTeclado = ( ) =>{
-    Keyboard.dismiss()//cierra el teclado al hacer click en la pantalla.
-  }
+    ciudad: "",
+    pasi: "",
+  });
+  const [consultar, setConsultar] = useState(false);
+  const [resultado, setResultado] = useState({});
+  const { ciudad, pais } = busqueda;
+  useEffect(() => {
+    const consultarClima = async () => {
+      if (consultar) {
+        const apiID = "45032e123978366dc405097237a267a3";
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${apiID}`;
+        console.log(url);
+        try {
+          const respuesta = await fetch(url);
+          const resul = await respuesta.json();
+          console.log(resul);
+          setResultado(resul);
+          setConsultar(false);
+        } catch (error) {
+          mostrarAlert();
+        }
+      }
+    };
+    consultarClima();
+  }, [consultar]);
+  const ocultarTeclado = () => {
+    Keyboard.dismiss(); //cierra el teclado al hacer click en la pantalla.
+  };
+  const mostrarAlert = () => {
+    Alert.alert("Error", "No hay resultados, intenta con otra ciudad o país", [
+      { text: "OK" },
+    ]);
+  };
   return (
     <>
-      <TouchableWithoutFeedback onPress={()=> ocultarTeclado()}>
+      <TouchableWithoutFeedback onPress={() => ocultarTeclado()}>
         <View style={styles.app}>
           <View style={styles.contenido}>
             <Formulario
-            busqueda={busqueda}
-            setBusqueda={setBusqueda}
+              busqueda={busqueda}
+              setBusqueda={setBusqueda}
+              setConsultar={setConsultar}
             />
           </View>
         </View>
